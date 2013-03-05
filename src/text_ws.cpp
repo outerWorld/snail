a999a4cc4069207dae31cfea0536ce67172db760
xxx.xxx.xxx.xxx <xxx-xxx-xxx-xxx@xxx-xxx-xxx-xxx.(none)>
2013-03-06 00:03:41 +0800

    text_ws

--- src/text_ws.cpp
+++ src/text_ws.cpp
@@ -46,10 +46,27 @@ int text_ws::parse_string(char *data, int data_len, unsigned int code_type)
 	int i = 0;
 	int res_count = 0;
 	LPICTCLAS_RESULT res;
+	sentence sent;
+	paragraph para;
+	char * p;
 
 	res = ICTCLAS_ParagraphProcessA(data, data_len, res_count, code_type, true);
-	
+
 	for (i=0; i<res_count; i++) {
+		p = res[i].iStartPos+data;
+		if (memcmp(p, "。", sizeof("。"))==0 || memcmp(p, "!", sizeof("!"))==0 || memcmp(p, "！", sizeof("！"))==0 || memcmp(p, ".", sizeof("."))==0) {
+			// if word is '。' or '!' , then a sentence ends.
+			para.push_back(sent);
+			sent.clear();
+		} else if (memcmp(p, "\n", 1)==0 || (memcmp(p, "\r\n", 2)==0)) {
+			// if word is '\n' or '\r\n', then a paragraph ends.
+			this->paragraphs.push_back(para);				
+			para.clear();
+		} else {
+			cn_word wd(p, res[i].iLength);
+			sent.push_back(wd);
+		}
+		sent.push_back(wd);
 	}
 
 	ICTCLAS_ResultFree(res);
@@ -58,4 +75,19 @@ int text_ws::parse_string(char *data, int data_len, unsigned int code_type)
 
 int text_ws::show_paragraph()
 {
+	vector<paragraph>::iterator para_ib;	
+	vector<sentence>::iterator sent_ib;
+	vector<cn_word>::iterator wd_ib;
+
+	for (para_ib=this->paragraphs.begin(); para_ib != this->paragraphs.end(); para_ib++) {
+		fprintf("Paragraph:\n");
+		for (sent_ib = para_ib->begin(); sent_ib!=para_ib->end(); sent_ib++) {
+			fprintf("\tSentence:\n\t");
+			for (wd_ib = sent_ib->begin(); wd_ib!=sent_ib->end(); wd_ib++) {
+				wd_ib->show();
+			}
+		}
+	}
+
+	return 0;
 }
