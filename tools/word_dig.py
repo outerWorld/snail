720d7ac9d8a65711a6a16ca62e93e806d1a64893
xxx.xxx.xxx.xxx <xxx-xxx-xxx-xxx@xxx-xxx-xxx-xxx.(none)>
2013-03-18 23:42:39 +0800

    add wd_prob, word_dig_config, punct, character_file

--- tools/word_dig.py
+++ tools/word_dig.py
@@ -3,16 +3,48 @@
 import sys
 import math
 import jieba.posseg as pseg
+from word_dig_config import *
 
 class punctuation():
 	def __init__(self):
 		pass
+
 	def load(self, punc_file):
-		pass
+		if not hasattr(self, 'punc_list'):
+			self.punc_list = { }
+		else:
+			self.punc_list.clear()
+
+		for line in open(punc_file, 'rb'):
+			line = line.decode('utf-8')
+			if line[0] == u'#':
+				continue
+			segs = line.strip(u'\n').split(u'/')
+			if len(segs) == 2:
+				# sentence flag with 's'
+				self.punc_list[segs[0]] = segs[1]
+			elif len(segs) == 1:
+				# not sentence flag
+				self.punc_list[segs[0]] = u'n'
+			else:
+				continue
+		self.punc_list[u'\n'] = u'n'
+
 	def is_sent_punct(self, wd):
-		pass
+		if wd in self.punc_list and self.punc_list[wd] == u's':
+			return True
+		else:
+			return False
+
 	def is_punct(self, wd):
-		pass
+		if wd in self.punc_list:
+			return True
+		else:
+			return False
+
+	def show(self):
+		for wd in self.punc_list:
+			print "%s/%s" %(wd, self.punc_list[wd])
 
 class word_digger():
 	def __init__(self, wd_prob_file):
@@ -26,7 +58,7 @@ class word_digger():
 
 	def load_wd_prob(self):
 		for line in open(self.wd_prob_file, 'r'):
-			segs = line.strip('\n').split()
+			segs = line.decode('utf-8').strip(u'\n').split(u' ')
 			num = len(segs)
 			if num < 2:
 				continue
@@ -45,49 +77,56 @@ class word_digger():
 			else:
 				continue
 
-	# sentence flag: . or ? or !
-	def chk_sent_flag(self, char):
-		if char == u'。' or char == u'.' or char == u'?' or char == u'？' or char == u'!' or char == u'！':
-			return True
-		else:
-			return False
-
-	# punctuation: .~`!@#$%^&*()-_=+;:'"/?.>,<
-	def chk_
-	
 	def file_ws(self, raw_file):
-		#words = pseg.cut(content)
 		if not hasattr(self, "wordlist"):
 			self.wordlist = {}
 		else:
 			self.wordlist.clear()
 
 		if not hasattr(self, "linelist"):
-			self.linelist = {}
+			self.linelist = []
 		else:
 			self.linelist.clear()
+		
+		chara_list = []
+		for line in open(character_file, 'rb'):
+			if line[0] == '#':
+				continue
+			chara_list.append(line.strip('\n').decode('utf-8'))
+
+		punct = punctuation()
+		punct.load(punct_config_file)
 
 		for line in open(raw_file, 'rb'):
 			data = []
-			p = line.decode('utf-8')
+			#print "%s" %(line.strip('\n'))
+			p = line.strip('\n').decode('utf-8')
+			#print "line = %s" %(p)
 			for i in range(0, len(p)):
-				print "word = %s" %(p[i])
-				if self.chk_sent_flag(p[i]):
-					if len(data) > 0:
-						print "sentence = [%s]" %(''.join(data))
-					data = []
+				#print "word=[%s]" %(p[i])
+				if punct.is_punct(p[i]):
+					if punct.is_sent_punct(p[i]):
+						if len(data) > 0:
+							self.linelist.append(''.join(data))
+							data = []
+						else:
+							pass
+					else:
+						pass
 				else:
 					data.append(p[i])
+			# if no sentence punctuation
+			if len(data) > 0:
+				self.linelist.append(''.join(data))
 
-		return 0
-
-		for wd in words:
-			wd_seg = wd.strip('\n').split('/')
-			if len(wd_seg) < 2:
-				continue
-			w = wd_seg[0]
-			pol = wd_seg[1]
-			
+		for line in self.linelist:
+			#print "%s" %(line)
+			words = list(pseg.cut(line.encode('utf-8')))
+			#print type(words)
+			for wd in words:
+				#print "w=%s pol=%s" %(wd.word, wd.flag)
+				for w in chara_list:
+					print "%s %s = %f" %(w, wd.word, self.calc_ir_score(wd.word, w))
 
 	# calculate interrelation score
 	# I(wd1, wd2) = log2(p(wd2|wd1)/p(wd2))
@@ -100,6 +139,7 @@ class word_digger():
 			# p(wd2|wd1)
 			if wd2 in self.wd_prob_dic[wd1]:
 				wd12_prob = self.wd_prob_dic[wd1][wd2]
+				print "[wd12_prob] = %f" %(wd12_prob)
 			else:
 				p2 = 0.0
 		else:
@@ -111,7 +151,7 @@ class word_digger():
 			wd2_prob = self.wd_prob_dic[wd2][wd2]
 
 		if wd2_prob == 0 or wd12_prob == 0:
-			return 0
+			return 0.0
 		else:
 			return math.log(wd12_prob/wd2_prob, 2)
 
@@ -127,8 +167,9 @@ class word_digger():
 		self.file_ws(raw_file)
 
 if __name__ == "__main__":
-	if len(sys.argv) > 2:
-		digger = word_digger(sys.argv[1])
+	if len(sys.argv) > 1:
+		digger = word_digger(wd_prob_config_file)
 		digger.load_wd_prob()
 		# digger.show_dic()
-		digger.wd_dig(sys.argv[2])
+		digger.set_threshold(glb_threshold_value)
+		digger.wd_dig(sys.argv[1])
---
+++ tools/word_dig_config.py
@@ -0,0 +1,5 @@
+# -*- encoding=utf-8 -*-
+punct_config_file = 'punct.conf'
+wd_prob_config_file = 'wd_prob.conf'
+glb_threshold_value = 1.4
+character_file = 'character_file'
